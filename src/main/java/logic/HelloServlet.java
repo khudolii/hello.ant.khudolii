2448444cb9c35fea8f4a7fb540cb97b334077066
evgeniy <hudoliy.e@gmail.com>
2020-11-05 08:46:44 -0500

    Add Hello project

---
+++ src/main/java/logic/HelloServlet.java
@@ -0,0 +1,17 @@
+package logic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Hello", urlPatterns = {"*.html"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
